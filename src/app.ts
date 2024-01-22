b7216f9794775ffe735abf97f1e6f945c186a3aa
Sourav Bhukta <souravbhukta8@gmail.com>
2024-01-22 12:45:04 +0530

    link

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(multer().any())
 
 app.use(cors({
-    origin: ["https://e-learning-frontend-one.vercel.app/"],
+    origin: ["https://e-learning-frontend-one.vercel.app"],
     credentials: true
 }));
 
